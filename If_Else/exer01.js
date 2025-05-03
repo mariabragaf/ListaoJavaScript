f70b9683446628d28c6da0632002e3ed603ee135
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-05-03 19:57:30 -0300

    positivo ou negativo

---
+++ If_Else/exer01.js
@@ -0,0 +1,9 @@
+let num = -5;
+
+if (num > 0){
+    console.log('o número é positivo');
+} else if (num < 0){
+    console.log('o número é negativo');
+} else {
+    console.log('o número é zero')
+}
