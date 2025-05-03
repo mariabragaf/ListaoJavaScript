4d149b088d99e1d370a43b3a733489668570f055
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-05-03 20:46:49 -0300

    clima

---
+++ If_Else/exer09.js
@@ -0,0 +1,9 @@
+let clima = 40;
+
+if (clima < 15){
+    console.log('o clima está frio');
+} else if (clima <= 25){
+    console.log('o clima está agradável');
+} else {
+    console.log('o clima está quente');
+}
