abe8c5dce7325cca55616fb0381d0cec6fe382bf
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-05-03 20:09:28 -0300

    classificação etária

---
+++ If_Else/exer03.js
@@ -0,0 +1,11 @@
+let idade = 67;
+
+if (idade >= 0 && idade <= 12){
+    console.log('criança');
+} else if (idade <= 17){
+    console.log('adolescente');
+} else if (idade <= 59){
+    console.log('adulto');
+} else {
+    console.log('idoso');
+}
