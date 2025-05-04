c8115d836c548e620416c2a61fb5c19a50fd2729
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-05-04 19:56:43 -0300

    estações do ano

---
+++ Switch_Case/exer25.js
@@ -0,0 +1,19 @@
+let estação = 1;
+
+switch (estação) {
+    case 1:
+        console.log('verão');
+        break;
+    case 2:
+        console.log('outono');
+        break;
+    case 3:
+        console.log('inverno');
+        break;
+    case 4:
+        console.log('primavera');
+        break;
+    default:
+        console.log('estação inválida');
+
+}
