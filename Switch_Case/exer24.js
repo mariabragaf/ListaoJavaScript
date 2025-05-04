e7cebe45f872621788525782ecbee26267984a8f
Maria Eduarda <maria.e.ferreira77@aluno.senai.br>
2025-05-04 19:50:27 -0300

    notas

---
+++ Switch_Case/exer24.js
@@ -0,0 +1,18 @@
+let nota = 10;
+
+switch (true) {
+    case nota >= 9:
+        console.log('a');
+        break;
+    case nota >= 7:
+        console.log('b');
+        break;
+    case nota >= 5:
+        console.log('c');
+        break;
+    case nota >= 3:
+        console.log('d');
+        break;
+    default:
+        console.log('e');
+}
